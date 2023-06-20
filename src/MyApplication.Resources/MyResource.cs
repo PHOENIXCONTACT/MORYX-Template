a847dc954dd5939994c5b14f9ca9b9be762b6d96
Thomas Fuchs <tfuchs@phoenixcontact.com>
2023-06-20 15:25:49 +0200

    Revert to MORYX app structure

---
+++ src/MyApplication.Resources/MyResource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Moryx.AbstractionLayer.Resources;
+using Moryx.Serialization;
+
+namespace MyApplication.Resources
+{
+    public class MyResource : Resource
+    {
+        [DataMember, EntrySerialize]
+        public int Number { get; set; }
+
+        [EntrySerialize, DisplayName("Do Foo")]
+        public void DoFoo()
+        {
+        }
+    }
+}
