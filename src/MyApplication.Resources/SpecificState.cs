ff82662b947b6803216688c7d9c6bcdbb8518406
Christian Siewert <csiewert@phoenixcontact.com>
2024-08-13 16:43:45 +0200

    Add state machine classes

---
+++ src/MyApplication.Resources/SpecificState.cs
@@ -0,0 +1,11 @@
+﻿using Moryx.StateMachines;
+
+namespace MyApplication.Resources.SomeCell.States
+{
+    internal class SpecificState : SomeStateBase
+    {
+        public SpecificState(SomeCell context, StateMap stateMap) : base(context, stateMap)
+        {
+        }
+    }
+}
---
+++ src/MyApplication.Resources/StateBase.cs
@@ -0,0 +1,13 @@
+﻿using Moryx.StateMachines;
+
+namespace MyApplication.Resources.SomeCell.States
+{
+    internal abstract class SomeStateBase : StateBase<SomeCell>
+    {
+
+        
+        public SomeStateBase(SomeCell context, StateMap stateMap) : base(context, stateMap)
+        {
+        }
+    }
+}
