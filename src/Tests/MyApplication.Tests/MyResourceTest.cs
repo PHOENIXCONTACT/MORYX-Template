85cef4ea58844b3e07c4c11ab97f255d38bd8556
Christian Siewert <csiewert@phoenixcontact.com>
2024-08-20 10:30:19 +0200

    Add plain single resource template

---
+++ src/Tests/MyApplication.Tests/MyResourceTest.cs
@@ -0,0 +1,22 @@
+﻿using MyApplication.Resources.MyResource;
+using NUnit.Framework;
+
+namespace MyApplication.Tests;
+
+[TestFixture]
+public class MyResourceTest
+{
+    [Test]
+    public void ResourceKeepsValue()
+    {
+        // Arrange
+        var myResource = new MyResource();
+        myResource.Value = 42;
+
+        // Act
+        myResource.Value = 1337;
+
+        // Assert
+        Assert.That(1337, Is.EqualTo(myResource.Value));
+    }
+}
