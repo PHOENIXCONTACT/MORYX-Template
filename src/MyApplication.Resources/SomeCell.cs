53fea8ef1b564991b83fff74c2b8cc6cb67be325
Marcel Vielhaus <mvielhaus@phoenixcontact.com>
2025-10-16 12:50:34 +0200

    Remove unused method

--- src/MyApplication.Resources/SomeCell.cs
+++ src/MyApplication.Resources/SomeCell.cs
@@ -74,10 +74,6 @@ public class SomeCell : Cell, IStateContext
         /* Start execution here */
     }
 
-    private void InstructionCompleted(int result, ActivityStart session)
-    {
-    }
-
     public override void ProcessAborting(IActivity affectedActivity)
     {
         // Default behavior: Clear instruction and abort execution
